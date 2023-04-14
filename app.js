d5769da11a6ef5d1e878039208a856d312f075bb
Henry Ayala <hayala@niksun.com>
2023-04-14 15:46:36 -0400

    started shopping list project

--- app.js
+++ app.js
@@ -1,30 +1,75 @@
 'use strict';
 
 angular.module('myFirstApp', [])
+.controller('MyFirstController', MyFirstController)
+.provider('ListService', ListServiceProvider)
+.filter('shortenName', ShortenNameFilter);
 
-.controller('MyFirstController', MyFirstController);
+MyFirstController.$inject = ['$filter', 'shortenNameFilter', 'ListService'];
 
-MyFirstController.$inject = ['$scope', '$filter'];
+function MyFirstController($filter, shortenNameFilter, ListService) {
+    this.name = '';
 
-function MyFirstController($scope, $filter) {
-    var upperCaseFilter = $filter('uppercase');
-    $scope.name = '';
-    $scope.nameValue = 0;
-
-    $scope.displayNameValue = () => {
-        $scope.nameValue = getNumericValue($scope.name);
-    };
-
-    $scope.blur = () => {
-        $scope.name = upperCaseFilter($scope.name);
-    };
-}
-
-function getNumericValue(input) {
-    let val = 0;
-    for (const s of input) {
-        val += input.charCodeAt(input.indexOf(s));
+    this.addToList = function () {
+        try {
+            ListService.addToList({ task: this.task, time: this.time });
+        }
+        catch(error) {
+            this.error = error.message;
+        }
     }
 
-    return val;
+    this.removeFromList = function(index) {
+        ListService.removeFromList(index);
+    }
+
+    this.getList = function () {
+        return ListService.getList();
+    }
+}
+
+function ListServiceProvider() {
+    var provider = this;
+
+    provider.defaults = {
+        quantity: 3
+    }
+
+    provider.$get = function () {
+        return new ListService(provider.defaults.quantity);
+    }
+}
+
+function ListService(amount) {
+    var todoList = [];
+    var maxQuantity = amount;
+
+    this.addToList = function (item) {
+        if (todoList.length < maxQuantity) {
+            todoList.push(item);
+        }
+        else {
+            throw new Error('Max quantity reached');
+        }
+    }
+
+    this.getList = function () {
+        return todoList;
+    }
+
+    this.removeFromList = function (index) {
+        todoList.splice(index, 1);
+    }
+}
+
+function ShortenNameFilter() {
+    return function(name) {
+        var splitName = name.split(' ');
+
+        if (splitName.length >= 2) {
+            splitName[1] = `${splitName[1][0]}.`;
+        }
+
+        return splitName.join(' ');
+    };
 }
